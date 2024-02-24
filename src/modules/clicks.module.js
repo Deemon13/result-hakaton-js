614c1a15daf2d88fdf7f1dc271ad0cca7c1d9d58
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2024-02-24 20:57:07 +0300

    fixed click module

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,26 +1,39 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module';
 
 export class ClicksModule extends Module {
-    constructor(type, text) {
-        super(type, text);
-        this.body = document.querySelector('body');
-        this.counterSingleClicks = 0;
-        this.counterDoubleCLicks = 0;
-    }
+  constructor(type, text, counterSingleClicks = 0, counterDoubleCLicks = 0) {
+    super(type, text);
+    this.body = document.querySelector('body');
+    this.body.addEventListener('click', this.handleOneClick.bind(this));
+    this.body.addEventListener('dblclick', this.handleDblClick.bind(this));
+    this.counterSingleClicks = counterSingleClicks;
+    this.counterDoubleCLicks = counterDoubleCLicks;
+  }
 
-    timerForClick() {
-        this.body.addEventListener('click', () => {
-            this.counterSingleClicks++;
-            let time = 2000;
-            let timerId = setTimeout(() => {
-                clearTimeout(timerId);
-                console.log(timerId);
-                alert(`Количество кликов за 2 секунды: ${this.counterSingleClicks}`);
-            }, time);
-        });
-    }
+  handleOneClick() {
+    this.counterSingleClicks += 1;
+  }
 
-    trigger() {
-        this.timerForClick();
+  handleDblClick() {
+    this.counterDoubleCLicks += 1;
+  }
+
+  trigger() {
+    this.timeForClick();
+  }
+
+  timeForClick() {
+    let time = 5000;
+    const timerID = setTimeout(() => {
+      this.body.removeEventListener('click', this.handleOneClick);
+      this.body.removeEventListener('dblclick', this.handleDblClick);
+
+      console.log('Время вышло!');
+      console.log('Одиночные клики', this.counterSingleClicks);
+      console.log('Двойные клики', this.counterDoubleCLicks);
+    }, time);
+    if (time <= 0) {
+      clearTimeout(timerID);
     }
+  }
 }
