94dc65ef5f1488dec55c672df54bcbd4e4ab7e9b
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2024-02-24 10:19:38 +0300

    fixed sounds now worked

--- src/modules/modules.js
+++ src/modules/modules.js
@@ -1,48 +1,23 @@
-import {
-  BackgroundModule,
-  //   BackgroundModule2,
-  //   BackgroundModule3,
-} from './background.module';
+import { BackgroundModule } from './background.module';
 
 import { ShapeModule } from './shape.module';
 import { ClicksModule } from './clicks.module';
 import { Timer } from './timer.module';
-//imported Message and Randsom Sound
 import { RandomSound } from './randomSound.module';
 import { Message } from './message.module';
-
-/*
-здесь добавить импорты экземпляров классов Таймер отсчета, Случайный звук, Кастомное сообщение
-*/
 
 export const clicksModule = new ClicksModule('click', 'Аналитика кликов');
 
 export const shapeModule = new ShapeModule('shape', 'Случайная фигура');
 
-/**/
 export const timer = new Timer('timer', 'Таймер отсчета');
-/**/
 
-/*
- здесь вставить export для экземпляра класса Случайный звук
-*/
-export const sound = new RandomSound ('звук', 'Случайный звук');
+export const sound = new RandomSound('звук', 'Случайный звук');
 
 export const backgroundModule = new BackgroundModule(
   'back',
   'Случайный фон',
   'body'
 );
-// export const backgroundModule2 = new BackgroundModule2(
-//   'back2',
-//   'Случайный фон2'
-// );
-// export const backgroundModule3 = new BackgroundModule3(
-//   'back3',
-//   'Случайный фон3'
-// );
 
-/*
- здесь вставить export для экземпляра класса Кастомное сообщение
-*/
-export const message = new Message ('сообщение', 'Кастомное сообщение');
+export const message = new Message('сообщение', 'Кастомное сообщение');
